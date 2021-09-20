695d628b9ee08f0946a6a48f3abe520320732863
user715 <61826786+user715@users.noreply.github.com>
2021-09-20 09:17:20 +0530

    Merge sort added

--- mainScript.js
+++ mainScript.js
@@ -28,12 +28,13 @@ var runBtn=document.getElementById("runBtn");
 runBtn.addEventListener("click",function (){
     var algoValue=getAlgo();
     disable();
+    delay=0;
     switch(algoValue)
     {
         case "0":bubbleSort();break;
         case "1":break;
         case "2":break;
-        case "3":break;
+        case "3":mergeSort(0,arrSize-1);break;
         case "4":break;
         case "5":break;
     }
@@ -41,7 +42,7 @@ runBtn.addEventListener("click",function (){
 
 
 var container=document.getElementById("container");
-var maxHeight=window.innerHeight-document.querySelector("header").style.height-300;
+var maxHeight=window.innerHeight-document.querySelector("header").style.height-150;
 var barWidth=10;
 
 var inputArr=document.getElementById("arraySize");
@@ -53,7 +54,7 @@ newArray();
 var speed=50;
 var inputSpeed=document.getElementById("speed");
 inputSpeed.addEventListener("input",function (){
-    speed=inputSpeed.value;
+    speed=110-inputSpeed.value;
 });
 
 
@@ -69,7 +70,6 @@ for(var i=0;i<arrSize;i++)
     temp.id=i.toString();
     temp.style.width="10px";
     temp.style.height=barHeight.toString()+"px";
-    temp.class="bar";
     temp.style.display="inline";
     temp.style.margin="2px";
     temp.style.float="left";
@@ -107,8 +107,6 @@ function swap(e1,e2)
 }
 
 function bubbleSort(){
-    disable();
-    delay=0;
     console.log("Bubble");
     var tempSize=arrSize;
     console.log(tempSize);
@@ -120,20 +118,98 @@ function bubbleSort(){
             var e2=document.getElementById((j+1).toString());
             setTimeout(Update,delay,e2,"rgb(255,0,0)");
             setTimeout(Update,delay,e1,"rgb(255,0,0)");
-            delay+=10-speed/10;
+            delay+=speed;
             setTimeout(swap,delay,e1,e2);
             delay+=10-speed/10;
             setTimeout(Update,delay,e2,"rgb(0,255,0)");
             setTimeout(Update,delay,e1,"rgb(0,255,0)");
-            delay+=10-speed/10;
+            delay+=speed;
         }
         setTimeout(last,delay,tempSize);
         tempSize--;
     }
     setTimeout(enable,delay);
 }
-
 function last(tempSize) {document.getElementById((tempSize-1).toString()).style.backgroundColor="rgb(0,0,255)"}
 ///BUBBLE SORT
 
 
+
+
+///MERGE SORT
+var obj={"idxL":0,"idxR":0,"arrIdx":0};
+
+function mergeUtil(sizeL,sizeR,Left,Right)
+{
+
+    if(obj.idxL<sizeL && obj.idxR<sizeR)
+    {
+        if(Left[obj.idxL]>Right[obj.idxR])
+        {
+            document.getElementById(obj.arrIdx.toString()).style.height=Right[obj.idxR].toString()+"px";
+            obj.idxR++;
+        }
+        else 
+        {
+            document.getElementById(obj.arrIdx.toString()).style.height=Left[obj.idxL].toString()+"px";
+            obj.idxL++;
+        }
+        obj.arrIdx++;
+    }
+    else if(obj.idxL<sizeL)
+    {
+        document.getElementById(obj.arrIdx.toString()).style.height=Left[obj.idxL].toString()+"px";
+        obj.idxL++;
+        obj.arrIdx++;
+    }
+    else if(obj.idxR<sizeR)
+    {
+        document.getElementById(obj.arrIdx.toString()).style.height=Right[obj.idxR].toString()+"px";
+        obj.idxR++;
+        obj.arrIdx++;
+    }
+}
+
+function merge(begin,mid,end)
+{
+    begin=parseInt(begin);
+    mid=parseInt(mid);
+    end=parseInt(end);
+    var m_delay=0;
+    var Left=[],Right=[];
+    var sizeL=mid-begin+1,sizeR=end-mid;
+    for(var i=0;i<sizeL;i++)Left[i]=parseInt(document.getElementById((begin+i).toString()).style.height.toString());
+    for(var i=0;i<sizeR;i++)Right[i]=parseInt(document.getElementById((mid+i+1).toString()).style.height.toString());
+    
+    obj.idxL=0;obj.idxR=0;obj.arrIdx=begin;
+    for(var i=0;i<sizeL+sizeR;i++)
+    {
+        setTimeout(mergeUtil,m_delay,sizeL,sizeR,Left,Right);
+        m_delay+=speed;
+    }
+}
+function mergeSort(begin,end)
+ {
+    begin=parseInt(begin);
+    end=parseInt(end);
+    if (begin >= end) return;
+  
+    var mid = Math.floor(begin + (end - begin) / 2);
+    mergeSort(begin, mid);
+    mergeSort(mid + 1, end);
+
+    setTimeout(merge,delay,begin,mid,end);
+
+    for(var i=0;i<end-begin+1;i++)
+    setTimeout(Update,delay,document.getElementById((begin+i),toString()) ,"rgb(255,0,0)");
+
+    delay+=(end-begin+5)*(speed);
+    if(begin==0 && end==arrSize-1)setTimeout(enable,delay);
+
+    for(var i=0;i<end-begin+1;i++)
+    {
+        if(begin==0 && end==arrSize-1)setTimeout(Update,delay,document.getElementById((begin+i),toString()) ,"rgb(0,0,255)");
+        else setTimeout(Update,delay,document.getElementById((begin+i),toString()) ,"rgb(0,255,0)");
+    }
+}
+////MERGE SORT
